82867237c799ac5796cb0030858d5852eb47bfc3
Sandeepcr8 <sandeep@createprotocol.org>
2023-01-08 18:42:23 +0530

    fix: navigation

--- Components/BottomNavbar.tsx
+++ Components/BottomNavbar.tsx
@@ -1,43 +1,53 @@
 "use client";
 import type { NextComponentType, NextPageContext } from "next";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { Chats, Heart, UserCircle, UsersThree } from "phosphor-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface Props {}
-
 const BottomNavbar: NextComponentType<NextPageContext, {}, Props> = (
   props: Props
 ) => {
-  const [currentTab, setCurrentTab] = useState("contact");
+  const [currentTab, setCurrentTab] = useState("");
+  const pathname = usePathname();
+  useEffect(() => {
+    if (pathname === null) {
+      setCurrentTab("/");
+    } else {
+      setCurrentTab(pathname);
+    }
+  }, []);
   return (
-    <div className="fixed bottom-0 bg-white py-5 w-full flex justify-around">
+    <div className="fixed bottom-0 flex w-full justify-around bg-white py-5">
       {/* <Heart color="#AE2983" weight="fill" size={32} /> */}
       <Link href="/contacts">
         <UsersThree
           onClick={() => {
-            setCurrentTab("contact");
+            setCurrentTab("/contacts");
           }}
           size={32}
-          color={`${currentTab === "contact" ? "rgb(126 34 206)" : "#000"}`}
+          color={`${currentTab === "/contacts" ? "rgb(126 34 206)" : "#000"}`}
         />
       </Link>
       <Link href="/">
         <Chats
           onClick={() => {
-            setCurrentTab("chat");
+            setCurrentTab("/");
           }}
           size={32}
-          color={`${currentTab === "chat" ? "rgb(126 34 206)" : "#000"}`}
+          color={`${currentTab === "/" ? "rgb(126 34 206)" : "#000"}`}
         />
       </Link>
       <Link href="/profile">
         <UserCircle
           onClick={() => {
-            setCurrentTab("profile");
+            setCurrentTab("/profile");
           }}
           size={32}
-          color={`${currentTab === "profile" ? "rgb(126 34 206)" : "#000"}`}
+          color={`${
+            currentTab.includes("profile") ? "rgb(126 34 206)" : "#000"
+          }`}
         />
       </Link>
     </div>
--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -2,9 +2,9 @@ import type { NextPage } from "next";
 
 const Contacts: NextPage = () => {
   return (
-    <>
+    <div>
       <h1>Contacts</h1>
-    </>
+    </div>
   );
 };
 
